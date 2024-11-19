1a7812b584a32b56852fc76def750bb5584374c3
Digvijay Pawar <pawardigvijay.rcpit@gmail.com>
2024-11-20 00:13:22 +0530

    Project uploaded to github

--- server/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addUser } from "../controllers/user.js";
-const router = express.Router();
-
-import upload from "../middleware/multer.js"; // Import multer config
-
-router.post("/registerUser", upload.single("aadhaarCard"), addUser);
-
-export default router;
